fb75220f776f2c11c56ace6248c57fec033b3bb2
Enzo Horquin <ehorquin@alumnos.exa.unicen.edu.ar>
2020-05-04 14:07:51 -0300

    Change config

--- config/config.js
+++ config/config.js
@@ -2,11 +2,10 @@ module.exports = {
     port: 3000,
     secret: "secret",
     mysql: {
-        host: process.env.CHALLENGE_MYSQL_HOST || "127.0.0.1",
-        user: process.env.CHALLENGE_MYSQL_USER || "root",
-        password: process.env.CHALLENGE_MYSQL_PASS || "38828881",
+        host: process.env.CHALLENGE_MYSQL_HOST || "host",
+        user: process.env.CHALLENGE_MYSQL_USER || "user",
+        password: process.env.CHALLENGE_MYSQL_PASS || "asdasd",
         database: process.env.CHALLENGE_MYSQL_DB || "pizza_system",
         connectionLimit: process.env.CHALLENGE_MYSQL_CONNLIMIT || 10
     },
 };
-
