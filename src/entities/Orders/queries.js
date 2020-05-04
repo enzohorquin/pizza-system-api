cfbab46e203e0ce2ba6809028f4f3d337b693259
Enzo Horquin <ehorquin@alumnos.exa.unicen.edu.ar>
2020-05-04 18:03:32 -0300

    Query fix

--- src/entities/Orders/queries.js
+++ src/entities/Orders/queries.js
@@ -1,6 +1,8 @@
 module.exports = {
     insertOrder: 'INSERT INTO orders (totalAmount, idAddress, idUser, note, dateOrder) VALUES (?, ?, ?, ?, ?)',
-    getOrders: 'SELECT o.totalAmount, o.dateOrder, p.name, po.quantity FROM orders o JOIN productinorder po ON (o.idOrder = po.idOrder) JOIN products p ON (po.idProduct = po.idProduct) WHERE o.idUser = ?',
+    getOrders: `SELECT o.totalAmount, o.dateOrder, p.name, po.quantity
+    FROM orders o JOIN productinorder po ON (o.idOrder = po.idOrder) JOIN products p ON (po.idProduct = p.idProduct)
+     WHERE o.idUser = ?`,
     insertProductOrder: 'INSERT INTO productinorder (idProduct, quantity, idOrder) VALUES ?',
     insertAddress: 'INSERT INTO addresses (street, apartment, idUser) VALUES (?, ?, ?)'
 }
